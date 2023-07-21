c84fb79c44ad2393202f89d45c3543112f0a5171
Alesrin <68646511+alesrin@users.noreply.github.com>
2023-07-21 13:50:17 +0200

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { LoginComponent } from './login/login.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireAuthModule,
+    AngularFireModule,
+    ReactiveFormsModule,
+    AngularFirestoreModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ejercicio-firebase-94254',
+    appId: '1:294867356098:web:f13642050d2ef329802e5b',
+    storageBucket: 'ejercicio-firebase-94254.appspot.com',
+    apiKey: 'AIzaSyAXtu8rZUFna2fhw5Jsb9d2WQQIvpkbVvE',
+    authDomain: 'ejercicio-firebase-94254.firebaseapp.com',
+    messagingSenderId: '294867356098',
+  },
   production: false
 };
 
